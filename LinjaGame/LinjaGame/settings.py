e846184ab2ada45f31e59d80709e6c0ef88d3bfe
erika quintana <erika.quintana@uptc.edu.co>
2021-02-27 17:22:06 -0500

    :construction: Movimiento de fichas en la vista

--- LinjaGame/LinjaGame/settings.py
+++ LinjaGame/LinjaGame/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'linjaengine',
-    'materializecssform'
+    'materializecssform',
+    'jquery'
 ]
 
 MIDDLEWARE = [
